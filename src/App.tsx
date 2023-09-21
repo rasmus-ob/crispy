6b5c296b6e20e74f4995a77d344f351da91d2051
Rasmus Olofsson Burman <rasmus_ob@outlook.com>
2023-09-21 16:03:17 +0200

    added footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <>
         <div className="container mx-auto">
             <Header />
+            <Footer />
         </div>
     </>
   )
---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer className='absolute font-bold text-center p-5 left-0 bottom-0 right-0'>
+            <p>Copyright &copy; 2023 | Created By Rasmus Olofsson Burman</p>
+        </footer>
+    )
+}
+
+export default Footer;
